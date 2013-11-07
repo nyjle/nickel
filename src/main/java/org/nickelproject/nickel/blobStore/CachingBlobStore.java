235277d47b7ff6e133c62ce136e1dcef9bc19a74
nigelduffy <nigel@nigelduffy.com>
2013-11-07 07:36:57 -0800

    Finished merge.

---
+++ src/main/java/org/nickelproject/nickel/blobStore/CachingBlobStore.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2013 Nigel Duffy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.nickelproject.nickel.blobStore;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.commons.io.IOUtils;
+import org.nickelproject.util.RethrownException;
+import org.nickelproject.util.RetryProxy;
+
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public final class CachingBlobStore implements BlobStore {
+    private final BlobStore                   blobStore;
+    private final LoadingCache<BlobRef, byte[]> cache = CacheBuilder.newBuilder().build(
+            new CacheLoader<BlobRef, byte[]>() {
+                @Override
+                public byte[] load(final BlobRef key) throws Exception {
+                    return blobStore.get(key);
+                }
+            });
+
+    @Inject
+    public CachingBlobStore(final BlobStore objectStore) {
+        this.blobStore = RetryProxy.newInstance(BlobStore.class, objectStore);
+    }
+
+    @Override
+    public boolean contains(final BlobRef blob) {
+        return cache.getIfPresent(blob) != null || blobStore.contains(blob);
+    }
+
+    @Override
+    public byte[] get(final BlobRef blob) {
+        try {
+            return contains(blob) ? cache.get(blob) : null;
+        } catch (final ExecutionException e) {
+            throw RethrownException.rethrow(e);
+        }
+    }
+
+    @Override
+    public BlobRef put(final byte[] data) {
+        final BlobRef blobRef = blobStore.put(data);
+        cache.put(blobRef, data);
+        return blobRef;
+    }
+
+    @Override
+    public InputStream getAsStream(final BlobRef blobRef) {
+        final byte[] bytes = get(blobRef);
+        return bytes == null ? null : new ByteArrayInputStream(bytes);
+    }
+
+    @Override
+    public BlobRef put(final InputStream stream) {
+        Preconditions.checkNotNull(stream);
+        try {
+            return put(IOUtils.toByteArray(stream));
+        } catch (final IOException e) {
+            throw RethrownException.rethrow(e);
+        }
+    }
+}
---
+++ src/main/java/org/nickelproject/nickel/blobStore/S3BlobStore.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2013 Nigel Duffy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.nickelproject.nickel.blobStore;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.nickelproject.util.RethrownException;
+
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+public class S3BlobStore extends BlobStoreBase {
+    private static final AmazonS3 s3Client   = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider());
+    private final String bucketName;
+
+    @Inject
+    S3BlobStore(@Named("BucketName") final String bucketName) {
+        this.bucketName = bucketName;
+        System.out.println(bucketName);
+    }
+
+    @Override
+    public final byte[] get(final BlobRef blobRef) {
+        InputStream inputStream = null;
+        try {
+            inputStream = getAsStream(blobRef);
+            return inputStream == null ? null : IOUtils.toByteArray(getAsStream(blobRef));
+        } catch (final IOException e) {
+            throw RethrownException.rethrow(e);
+        } finally {
+           if (inputStream != null) {
+               try {
+                   inputStream.close();
+               } catch (final IOException e) {
+                   throw RethrownException.rethrow(e);
+               }
+           }
+        }
+    }
+
+    @Override
+    public final boolean contains(final BlobRef blobRef) {
+        boolean retVal = true;
+        try {
+            s3Client.getObjectMetadata(bucketName, blobRef.toString());
+        } catch (final AmazonS3Exception e) {
+            retVal = false;
+        }
+        return retVal;
+    }
+
+    @Override
+    public final InputStream getAsStream(final BlobRef blobRef) {
+        return contains(blobRef) ? s3Client.getObject(bucketName, blobRef.toString()).getObjectContent() : null;
+    }
+
+    @Override
+    public final void putByteArray(final BlobRef blobRef, final byte[] pBytes) {
+        final ByteArrayInputStream vByteArrayInputStream = new ByteArrayInputStream(pBytes);
+        final ObjectMetadata vMetadata = new ObjectMetadata();
+        vMetadata.setContentLength(pBytes.length);
+        s3Client.putObject(new PutObjectRequest(bucketName, blobRef.toString(), vByteArrayInputStream, vMetadata));
+    }
+}
