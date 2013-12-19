fbf318f0fe78c812c17b8ddd44ebda84dad47edd
nigelduffy <nigel@nigelduffy.com>
2013-12-18 20:14:29 -0800

    Added a MinReducer

---
+++ src/main/java/org/nickelproject/util/reducers/MinReducer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013 Nigel Duffy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.nickelproject.util.reducers;
+
+import org.nickelproject.nickel.dataflow.Reducer;
+import org.nickelproject.nickel.dataflow.Reductor;
+
+public final class MinReducer<T extends Comparable<T>> implements Reducer<T, T> {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Reductor<T, T> reductor() {
+        return new MinReductor();
+    }
+
+    private class MinReductor implements Reductor<T, T> {
+        private T min = null;
+
+        @Override
+        public void collect(final T pVal) {
+            min = min == null ? pVal : pVal.compareTo(min) < 0 ? pVal : min;
+        }
+
+        @Override
+        public T reduce() {
+            return min;
+        }
+    }
+}
