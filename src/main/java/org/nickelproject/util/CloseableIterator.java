7bb5fe12e9d3f55826e58d6209f56fff2c91309e
nigelduffy <nigel@nigelduffy.com>
2014-10-13 22:16:48 -0700

    Fixed copyright notices. Fixed bad dependency for AWS.

--- src/main/java/org/nickelproject/util/CloseableIterator.java
+++ src/main/java/org/nickelproject/util/CloseableIterator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Nigel Duffy
+ * Copyright (c) 2013, 2014 Nigel Duffy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
